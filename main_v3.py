a71944f674d30c663bc473c57644fabe6118b1d0
Fernando Puga <19002228@alumnos.utleon.edu.mx>
2023-01-26 23:17:23 -0600

    Tercer commit de clase

--- main_v3.py
+++ main_v3.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app=Flask(__name__)
 
@@ -25,6 +26,23 @@ def func(id,name):
 @app.route("/suma/<float:n1>/<float:n2>")
 def suma(n1,n2):
     return "La suma es: {}".format(n1+n2)
+
+@app.route("/suma2",methods=["GET","POST"])
+def sumar():
+    if request.method=="POST":
+        num1=request.form.get("num1")
+        num2=request.form.get("num2")
+        return "<h2> La suma es: {}</h2>".format(str(int(num1)+int(num2)))
+    else:
+        return '''
+            <form action="/suma2" method="POST">
+            <label>N1: </label>
+            <input type="text" name="num1"/><br><br>
+            <label>N2: </label>
+            <input type="text" name="num2"/><br><br>
+            <input type="submit" value="calcular"/>
+            </form>
+            '''
 
 if __name__=="__main__":
     app.run(debug=True,port=3000)
